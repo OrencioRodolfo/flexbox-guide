84f3bfe8c01c0f7526455e7a3ae099a1198a7c93
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-04-05 23:36:30 +0100

    Rename 'app' folder to 'assets'

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,18 @@ const moduleImporter = require('sass-module-importer');
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-      server: "./app"
+      server: "./"
     });
 
-    gulp.watch("app/scss/**/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("assets/scss/**/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("app/scss/main.scss")
+  return gulp.src("assets/scss/main.scss")
     .pipe(sass({ importer: moduleImporter() }))
-    .pipe(gulp.dest("app/css"))
+    .pipe(gulp.dest("assets/css"))
     .pipe(browserSync.stream());
 });
 
