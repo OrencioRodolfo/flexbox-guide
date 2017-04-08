31a55fd988a8c6689c55021b72fc562c68ebca89
orenciorodolfo <orenciorodolfo@hotmail.com>
2017-04-08 15:47:40 +0100

    Add missing templates

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$.when(
+  loadHbTemplate('head'),
+  loadHbTemplate('introduction'),
+  loadHbTemplate('syllabus'),
+  loadHbTemplate('browser-support'),
+  loadHbTemplate('basics')
+).done(function(
+  headTpl,
+  introTpl,
+  syllabusTpl,
+  browserSupportTpl,
+  basicsTpl
+) {
+  $('#page-head').html(headTpl);
+  $('#introduction').html(introTpl);
+  $('#syllabus').html(syllabusTpl);
+  $('#browser-support').html(browserSupportTpl);
+  $('#basics-terminology').html(basicsTpl);
+});
+
+/**
+ * Loads Handlebars templates
+ */
+function loadHbTemplate(path, data) {
+  var templateData = data || {};
+
+  return new Promise(function(resolve) {
+    var request = $.get('partials/' + path + '.hbs', templateData, function(response) {
+      var template = Handlebars.compile(response);
+      resolve(template(templateData));
+    })
+  });
+}
